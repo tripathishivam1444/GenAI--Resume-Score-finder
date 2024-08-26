c9d7a7f991c8241df9d80dfb2f58667790f38761
Utkarsh Tripathi <66895751+tripathishivam1444@users.noreply.github.com>
2024-08-26 20:27:49 +0530

    Update streamlit_UI.py

--- streamlit_UI.py
+++ streamlit_UI.py
@@ -1,29 +1,32 @@
 import streamlit as st
-from streamlit_backend import pdf_docs, chroma_vector_store, similarity_search, delete_all_ids
+from streamlit_backend import pdf_docs, qdrant_vector_store, similarity_search, delete_all_ids
+
+import streamlit as st
 
 def main():
+    st.markdown("<h1 style='color:#800000;'>TRIPATHI   UTKARSH</h1>", unsafe_allow_html=True)
     st.title("Resume Similarity Score Finder")
 
-    # Input fields for text and number of outputs
     input_text = st.text_input("Enter the text to search for similarity:")
     no_of_outfiles = st.number_input("Enter the number of similar resumes to fetch:", min_value=1, max_value=10)
 
-    # File uploader for optional PDF uploads
     uploaded_files = st.file_uploader("Upload PDF files", type="pdf", accept_multiple_files=True)
 
     if st.button("Get Data"):
-        documents = []
-        if uploaded_files:
+        if not input_text:
+            st.warning("Please enter Job Description OR text to search for similarity.")
+        if not no_of_outfiles:
+            st.warning("Please enter the number of similar resumes to fetch.")
+        if not uploaded_files:
+            st.warning("Please upload at least one PDF file.")
+
+        if input_text and no_of_outfiles and uploaded_files:
             documents = pdf_docs(uploaded_files)
+            vector_store = qdrant_vector_store(documents)
 
-        vector_store = chroma_vector_store(documents if documents else None)
-
-        # Perform similarity search and display results
-        if input_text and no_of_outfiles > 0:
             result_dict = similarity_search(vector_store, input_text, no_of_outfiles)
             st.write(result_dict)
 
-            # Clear old documents from the vector store
             answer = delete_all_ids()
             st.write(answer)
 
